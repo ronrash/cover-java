33fb31882305e681a5826092df9e4e4492511049
Rohit Prashar <rohit.prashar@hmcts.net>
2023-04-21 09:05:01 +0100

    fibo

--- src/main/java/recursion/RecursionSample.java
+++ src/main/java/recursion/RecursionSample.java
@@ -5,4 +5,13 @@ public class RecursionSample {
     // recursion is a function calling itself
     // uses space
     // space complexity is not constant because every function call takes up memory and they are recursive
+
+    //
+    public static void main(String[] args) {
+        int ans = fib(4);
+    }
+
+    private static int fib(final int i) {
+        // base condition
+    }
 }
